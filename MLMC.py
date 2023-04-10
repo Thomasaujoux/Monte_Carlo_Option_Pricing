754bc8f0ce481025c087da83d956b806546ce314
Suzie14 <115775861+Suzie14@users.noreply.github.com>
2023-04-10 16:03:17 +0200

    Update MLMC.py

--- MLMC.py
+++ MLMC.py
@@ -2,85 +2,67 @@ import CIR
 import ordinaryMC
 import numpy as np
 
-def MLMC(L, N, M, alpha, beta, gamma, f):
-    """
-    Implements the multi-level Monte Carlo algorithm for function f.
-    
-    L: number of levels
-    N: number of samples at each level
-    M: number of simulations
-    alpha: convergence rate of weak error
-    beta: convergence rate of variance
-    gamma: bias reduction factor
-    f: function to be estimated
-    """
-    
-    # Compute the values of delta and epsilon for each level
-    delta = np.sqrt(beta / (2 * L))
-    eps = np.sqrt(alpha / (2 * L))
-    
-    # Initialize arrays for sample means and variances
-    means = np.zeros(L)
-    variances = np.zeros(L)
-    
-    for l in range(L):
-        # Compute the number of samples needed at each level
-        n_l = int(np.ceil((delta / eps) ** 2 * (2 ** l)))
-        
-        # Perform M simulations at each level with N samples
-        samples = np.zeros((M, n_l))
-        for m in range(M):
-            samples[m] = np.random.normal(size=n_l)
-        values = np.array([f(sample) for sample in samples])
-        
-        # Compute the sample mean and variance at each level
-        means[l] = np.mean(values)
-        variances[l] = np.var(values)
-    
-    # Compute the MLMC estimate
-    summands = np.array([(means[l] - means[l-1]) / (eps * np.sqrt(variances[l-1])) for l in range(1, L)])
-    mlmc_est = means[0] + gamma * np.sum(summands)
-    
-    return mlmc_est
 
-def optimalL(k, S_0, T, r, sigma, K, alpha, b):
+def h_l(k,T,l):
+    """
+    Calcul  de h_l
+    
+    k: number of simulations
+    T (float): Time period of option contract
+    l (int): indice of level
+    
+    output : h_l (float)
+    """
+    h_l = k**(-l)*T
+    return h_l
+
+def N_l(variance, k, M,l, L, epsilon):
+    
+    
+    
+    sum_vh = 0 
+    for l in range(0,L)
+        sum_vh = sum_vh + np.sqrt(variance[l]/h_l(k,M,l))
+        
+    N_l = int(np.ceil(2*epsilon**(-2)*np.sqrt(variance[l]*h_l(k,M,l))*sum_vh))
+                      
+    return N_l
+
+def sim_MLMC(k, S_0, T, r, sigma, K, alpha, b):
+    
+    #1) start with L=0
     L = 0 
-    N_L = 10**4
-    V_L = 
-
-def MLMC(nb_samples, k, S_0, T, r, sigma, K, alpha, b, L):
-    """
-    Conducts MLMC method,
+    N = []
+    epsilon = np.exp(-1) #fixé de cette façon dans le papier
     
-    INPUT:
-        nb_samples (int): Number of samples in simulation
-        k (int): Number of price step we aim to simulate in each path
-        S_0 (float): Underlying asset price at time zero
-        T (float): Time period of option contract
-        r (float): Risk-netural interest rate
-        sigma (float): Volatility in the environment
-        K (float): Exercise price of the option
-        alpha (float): taux de convergence
-        b (float): taux de convergence
+    while convergence==False or L < 2:
+        # Initialize arrays for sample means and variances
+        means = np.zeros(L)
+        variances = np.zeros(L)
+        for l in range(L+1):
+            N.append(10**4)
+            multiCIR_ML = CIR.multiCIR_ML(alpha, b, sigma, T, k, S_0, N[l], L)
+            values = np.array([ordinaryMC.pv_calc(ordinaryMC.payoff_calc(sample, K), r, T) for sample in multiCIR_ML])
+    
         
-    OUTPUT:
-        (Numpy.ndarray): A one-dimensional array of present value of simulated payoffs
-    """
-    present_payoffs = np.zeros(nb_samples)
-    multiCIR_ML = CIR.multiCIR_ML(alpha, b, sigma, T, k, S_0, nb_samples, L)
+            # Compute the sample mean and variance at each level
+            means[l] = np.mean(values)
+            variances[l] = np.var(values) #2) estimate VL using an initial N_L = 10**4 samples
     
-    for i in range(nb_samples):
-        present_payoffs[i] = ordinaryMC.pv_calc(ordinaryMC.payoff_calc(multiCIR[i], K), r, T)
-    return(present_payoffs)
-
-
-
-def ml_mc_sim(nb_samples, k, S_0, T, r, sigma, K, alpha, b):
-    L = 0
-    sim = []
-    while 'condition de convergence pas respectée'
-        L = L+1
-        sim[l] = MLMC(nb_samples, k, S_0, T, r, sigma, K, alpha, b, L)
-        values = np.array([present_payoffs for sample in samples])
+            #3) define optimal N_l, l = 0,...,L using Eqn. (12)
+            New = N_l(variance, k, M,l, L, epsilon)
     
+        #4)evaluate extra samples at each level as needed for new N_l
+            if New > N[l]:
+                Bis_multiCIR_ML = CIR.multiCIR_ML(alpha, b, sigma, T, k, S_0, New-N[l], L)
+                values = np.concatenate((multiCIR_ML,Bis_multiCIR_ML))
+                N[l] = New
+                
+                
+                
+            if np.abs(means[L] - k**(-1)*means[L-1]) < 1/np.sqrt(2)*((k**2)-1)**epsilon:
+                convergence=True
+            else :
+                L = L+1
     
+    return values, L, N
