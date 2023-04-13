32a3cbf1c82cd5b6a8780cdb152434e3117de90e
Thomasaujoux <121901631+Thomasaujoux@users.noreply.github.com>
2023-04-13 15:05:33 +0200

    Update CIR.py

--- CIR.py
+++ CIR.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy.stats import norm
 from scipy.optimize import minimize
+import chaospy as ch
+from scipy import stats
+
+
 
 
 def CIR(alpha, b, sigma, T, k, S_0):
@@ -22,11 +26,11 @@ def multiCIR(alpha, b, sigma, T, k, S_0, nb_samples):
 
 def CIR_ML(alpha, b, sigma, T, k, S_0, L):
     delta = np.zeros(L+1)
-    for l in range(0,L+1):
+    for l in range(0,L):
         delta[l] = 2 ** (-l)
     S = np.zeros(k+1)
     S[0] = S_0
-    for l in range(0,L+1):
+    for l in range(0,L):
         for i in range(1, k+1):
             S[i] = delta[l] * alpha * (b - S[i-1]) + sigma * np.sqrt(S[i-1]) * np.random.normal(scale=delta[l]) + S[i-1]
             if S[i] < 0 : S[i] = "Erreur"
@@ -36,4 +40,41 @@ def multiCIR_ML(alpha, b, sigma, T, k, S_0, nb_samples, L):
     multiCIR = []
     for i in range(nb_samples): 
         multiCIR.append(CIR_ML(alpha, b, sigma, T, k, S_0,L))
+    return multiCIR
+
+
+def sobol_generator(nb_samples, k):
+    """
+    Generates m samples (each for one path) each having n numbers in Sobol sequence.
+    
+    INPUT:
+        m (int): number of samples
+        n (int): number of Sobol sequence numbers in each sample
+        
+    OUTPUT:
+        (numpy.ndarray): A two-dimensional array of Sobol sequence numbers for conducting QMC simulation
+    """
+    sob_array = np.empty((0, k))
+    for i in range(nb_samples // 39 + 1):
+        sob = ch.create_sobol_samples(k, 39, i)
+        sob_array = np.append(sob_array, sob, axis=0)
+    return sob_array
+
+
+def multiCIR_QMC(alpha, b, sigma, T, k, S_0, nb_samples):
+    
+    dt = T/k
+    multiCIR = []
+    
+    epsilon_s = stats.norm.ppf(sobol_generator(nb_samples, k+1))
+    
+    for j in range(nb_samples):
+        S = np.zeros(k+1)
+        S[0] = S_0
+            
+        for i in range(1, k+1):
+            dS = alpha * (b - S[i-1]) * dt + sigma * np.sqrt(S[i-1] * dt) * epsilon_s[j, i]
+            S[i] = S[i-1]+ dS
+                
+        multiCIR.append(S)
     return multiCIR
