ae4ecc1ece80a6380a765ca25d30e9596ce423b9
elenalmg <elena.loumagne@gmail.com>
2023-04-22 18:05:25 +0200

    ooooover

--- MLMC.py
+++ MLMC.py
@@ -3,12 +3,12 @@ import ordinaryMC
 import numpy as np
 
 
-def h_l(k,T,l):
+def h_l(T,l):
     """
     Calcul  de h_l
     
     k: number of simulations
-    T (float): Time period of option contract
+    T (float): Time period of optsion contract
     l (int): indice of level
     
     output : h_l (float)
@@ -22,9 +22,9 @@ def N_l(variance, k, T,l, L, epsilon):
     
     sum_vh = 0 
     for level in range(0,L+1):
-        sum_vh = sum_vh + np.sqrt(variance[level]/h_l(k,T,l))
+        sum_vh = sum_vh + np.sqrt(variance[level]/h_l(T,l))
     
-    N_l = int(np.ceil(2*epsilon**(-2)*np.sqrt(variance[l]*h_l(k,T,l))*sum_vh))
+    N_l = int(np.ceil(2*epsilon**(-2)*np.sqrt(variance[l]*h_l(T,l))*sum_vh))
                       
     return N_l
 
@@ -65,46 +65,41 @@ def sim_MLMC(k, S_0, T, r, sigma, K, alpha, b):
     # Initialize arrays for sample means and variances
     
 
-    
     while convergence==False or L < 2:
 
         N.append(100)
             
         # Compute the sample mean and variance at each level
         values.append(sim_MLMC_Lfixe( S_0, T, r, sigma, K, alpha, b, N[L],L))
+        means = np.zeros(L+1)
+        variances = np.zeros(L+1)
         
         for l in range(L+1):
-            means = np.zeros(L+1)
-            variances = np.zeros(L+1)
+            
             means[l]=np.mean(values[l])
             variances[l] = np.var(values[l]) #2) estimate VL using an initial N_L = 10**4 samples
             
             
             #3) define optimal N_l, l = 0,...,L using Eqn. (12)
             New = N_l(variances, k, T, l, L, epsilon)
-            print(New)
-            print(N[l])
+      
         #4)evaluate extra samples at each level as needed for new N_l
             if New > N[l]:
-                print("yo")
                 values[l] = values[l]+sim_MLMC_Lfixe( S_0, T, r, sigma, K, alpha, b, New - N[l],l)
                 N[l] = New
 
         if L>=2:
             if np.abs(means[L] - 2**(-1)*means[L-1]) < 1/np.sqrt(2)*((2**2)-1)*epsilon:
-                print("conv")
                 convergence=True
                 
             else:
-                print("coucou")
                 L = L+1
-                print(L)
+                
         else: 
-            print("L trop petit")
             L = L+1
-           
-    
-    return  L, N, variances, means
+          
+    y_chap = np.sum(means)
+    return  y_chap, L, N, variances, means
 
 
 def sim_MLMC_Lfixe( S_0, T, r, sigma, K, alpha, b, N,L):
@@ -116,7 +111,6 @@ def sim_MLMC_Lfixe( S_0, T, r, sigma, K, alpha, b, N,L):
             a=ordinaryMC.pv_calc(ordinaryMC.payoff_calc(multicir[i][1],K),r,T)
             payoff_level.append(a)
        
-
     else:
           
         multicir=CIR.multiCIR_ML(alpha, b, sigma, L, T, S_0, N)
